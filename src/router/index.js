b371faa0c8d787d5e9e4e92d9aeac668bb5002fe
takurooper <taku.kuro@gmail.com>
2019-04-18 18:27:32 +0900

    add signin, signup pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import IndexPage from "@/components/App";
+import CrimeMap from '@/components/CrimeMap'
+import Signup from '@/components/Signup'
+import Signin from '@/components/Signin'
 
 Vue.use(Router)
 
 export default new Router({
-  data: {
-    todos: []
-  },
   routes: [
     {
       path: '/',
-      name: 'App',
-      component: IndexPage
+      name: 'Crime Map',
+      component: CrimeMap
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
