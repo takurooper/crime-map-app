a7ed4ecc2c51e3f84174850e5fe0b412891b47a4
takurooper <taku.kuro@gmail.com>
2019-04-18 18:45:41 +0900

    add logo

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="Index">
-        <p>犯罪防止アプリ</p> 
-        <router-view/>
-    </div>
+  <div id="app">
+    <img src="@/assets/logo.png">
+    <router-view/>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
