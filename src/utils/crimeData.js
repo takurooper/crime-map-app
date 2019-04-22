a8d71f3880435eb9c9f5f71d13981f9e61a0083e
maru919 <maruoka-kento757@g.ecc.u-tokyo.ac.jp>
2019-04-22 17:37:04 +0900

    東京都の犯罪件数を町ごとに渡せるように

---
+++ src/utils/crimeData.js
@@ -0,0 +1,16 @@
+const API_KEY = 'AIzaSyAiOIgnzJ1jGnHs9OoZpHxaae44XYlr_8w';
+
+const fs = require('fs');
+const csvSync = require('csv-parse/lib/sync');
+
+const crimeData = csvSync(fs.readFileSync('../../static/crimeDataH31.csv'))
+  .filter((town, index) => index !== 0 && !town[0].includes('計'))
+  .map(town => town.slice(0, 2));
+
+crimeData.forEach(town => {
+  town[1] = Number(town[1]);
+});
+
+export default {
+  crimeData
+}
