361882ac1112b56a5d17626c44f8b595b4a5e97f
takurooper <taku.kuro@gmail.com>
2019-04-25 17:19:57 +0900

    add child and parent

--- src/components/App.vue
+++ src/components/App.vue
@@ -8,12 +8,39 @@
 <script>
 /* eslint-disable */
 
+import firebase from 'firebase'
 import headerItem from './HeaderItem'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      database: null,
+      usersRef: null,
+      statusType: ['parent', 'child'],
+    }
+  },
+  created: function() {
+    this.database = firebase.database();
+    this.usersRef = this.database.ref('users', 'message');
+
+    var _this = this;
+    this.usersRef.on('value', function(snapshot) {
+      _this.users = snapshot.val(); // データに変化が起きたときに再取得する
+    });
+  },
   components : {
-    headerItem
+    headerItem,
+    child: require('./SignupForm')
+  },
+  methods: {
+    createStatus: function() {
+      if (this.statusType == "") { return; }
+      this.usersRef.push({
+        name: this.statusType
+      })
+      this.statusType = "";
+    }
   }
 }
 </script>
