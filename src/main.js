ee8c6cfc30f55730e1252ebe4e4c40557a2e464d
maru919 <maruoka-kento757@g.ecc.u-tokyo.ac.jp>
2019-04-25 13:58:04 +0900

    vue2-google-mapsパッケージを導入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './components/App'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAiOIgnzJ1jGnHs9OoZpHxaae44XYlr_8w',
+    libraries: 'places'
+  }
+})
 
 Vue.config.productionTip = false
 
